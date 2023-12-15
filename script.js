8072235d3bef35d31c461416f8541c776b9a0ab8
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-15 19:03:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
 function minDate(dates) {
-  //write you code here
+    if (!Array.isArray(dates) || dates.length === 0) {
+        throw new Error("Invalid input. Please provide a non-empty array of date strings.");
+    }
+
+    // Initialize with the first date in the array
+    let earliestDate = new Date(dates[0]);
+
+    // Iterate through the remaining dates and update earliestDate if a smaller date is found
+    for (let i = 1; i < dates.length; i++) {
+        const currentDate = new Date(dates[i]);
+
+        if (currentDate < earliestDate) {
+            earliestDate = currentDate;
+        }
+    }
+
+    // Format the result as "YYYY/MM/DD"
+    const formattedResult = `${earliestDate.getFullYear()}/${padNumber(earliestDate.getMonth() + 1)}/${padNumber(earliestDate.getDate())}`;
+
+    return formattedResult;
 }
 
-// Do not change the code
+// Helper function to pad single-digit numbers with a leading zero
+function padNumber(num) {
+    return num.toString().padStart(2, '0');
+}
 
-var dates = [
-  "2023/03/01",
-  "2023/03/02",
-  "2023/03/03",
-  "2023/03/04",
-  "2023/03/05",
-  "2023/03/06",
-  "2023/03/07",
-  "2023/03/08",
-  "2023/03/09",
-  "2023/03/10",
-  "2023/03/11",
-  "2023/03/12",
-  "2023/03/13",
-  "2023/03/14",
-  "2023/03/15",
-  "2023/03/16",
-  "2023/03/17",
-  "2023/03/18",
-  "2023/03/19",
-  "2023/03/20",
-  "2023/03/21",
-  "2023/03/22",
-  "2023/03/23",
-  "2023/03/24",
-  "2023/03/25",
-  "2023/03/26",
-  "2023/03/27",
-  "2023/03/28",
-  "2023/03/29",
-  "2023/03/30",
-];
-
-alert(minDate(dates));
